e6dfeeff26c277980debabd20ae3459f34be0053
PZerua <pachus.wtf@gmail.com>
2015-10-30 17:01:07 +0100

    Memory improved

--- GameVoid/include/Memory.h
+++ GameVoid/include/Memory.h
@@ -2,13 +2,52 @@
 #include "Header.h"
 #include "Cartridge.h"
 
-#define MEM_SIZE 65535
+#define MEM_SIZE 65536
+
+// Timer and Divider Registers
+#define TIMA	0xFF05
+#define TMA		0xFF06
+#define TAC		0xFF07
+// Sound Registers
+#define NR10	0xFF10
+#define NR11	0xFF11
+#define NR12	0xFF12
+#define NR14	0xFF14
+#define NR21	0xFF16
+#define NR22	0xFF17
+#define NR24	0xFF19
+#define NR30	0xFF1A
+#define NR31	0xFF1B
+#define NR32	0xFF1C
+#define NR33	0xFF1E
+#define NR41	0xFF20
+#define NR42	0xFF21
+#define NR43	0xFF22
+#define NR44	0xFF23
+#define NR50	0xFF24
+#define NR51	0xFF25
+#define NR52	0xFF26
+// LCD Control Registers
+#define LCDC	0xFF40
+#define SCY		0xFF42
+#define SCX		0xFF43
+#define LYC		0xFF45
+// LCD Monochrome Palettes
+#define BGP		0xFF47
+#define OBP0	0xFF48
+#define OBP1	0xFF49
+// LCD Position and Scrolling
+#define WY		0xFF4A
+#define WX		0xFF4B
+// Interrupt Register
+#define IE		0xFFFF
 
 class Memory
 {
 public:
 	Memory(Cartridge *cartridge);
 	~Memory();
+	void reset();
 
 private:
 	BYTE _MEMORY[MEM_SIZE];
--- GameVoid/src/Memory.cpp
+++ GameVoid/src/Memory.cpp
@@ -2,7 +2,11 @@
 
 Memory::Memory(Cartridge *cartridge)
 {
+	// Receibe Cartridge data
 	_cartridge = cartridge;
+
+	// Init Memory
+	reset();
 }
 
 Memory::~Memory()
@@ -44,4 +48,32 @@ void Memory::write(const WORD &address, const BYTE &value)
 
 	// Write to memory
 	_MEMORY[address] = value;
+}
+
+void Memory::reset()
+{
+	// Set all values to 0x00
+	memset(_MEMORY, 0x00, MEM_SIZE);
+
+	// Reset register default values (only those != 0x00)
+	_MEMORY[NR10] = 0x80;
+	_MEMORY[NR11] = 0xBF;
+	_MEMORY[NR12] = 0xF3;
+	_MEMORY[NR14] = 0xBF;
+	_MEMORY[NR21] = 0x3F;
+	_MEMORY[NR24] = 0xBF;
+	_MEMORY[NR30] = 0x7F;
+	_MEMORY[NR31] = 0xFF;
+	_MEMORY[NR32] = 0x9F;
+	_MEMORY[NR33] = 0xBF;
+	_MEMORY[NR41] = 0xFF;
+	_MEMORY[NR44] = 0xBF;
+	_MEMORY[NR50] = 0x77;
+	_MEMORY[NR51] = 0xF3;
+	_MEMORY[NR52] = 0xF1;
+	_MEMORY[LCDC] = 0x91;
+	_MEMORY[BGP]  = 0xFC;
+	_MEMORY[OBP0] = 0xFF;
+	_MEMORY[OBP1] = 0xFF;
+
 }
