1ba30021de63771a629852a215e742771c1f65c9
PLGar <pablolgarcia95@gmail.com>
2021-02-22 15:53:22 +0100

    fix cmd execution

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,18 @@
 int main(int argc, char* argv[])
 {
 
-//     if (argc == 1) {
-// #ifdef __linux__
-//         std::cout << "Usage: \nGameVoid <path to rom>" << std::endl;
-// #elif _WIN32
-//         std::cout << "Usage: \nGameVoid.exe <path to rom>" << std::endl;
-// #endif
-//         system("pause");
-//         return 0;
-//     }
+    if (argc == 1) {
+#ifdef __linux__
+        std::cout << "Usage: \nGameVoid <path to rom>" << std::endl;
+#elif _WIN32
+        std::cout << "Usage: \nGameVoid.exe <path to rom>" << std::endl;
+#endif
+        system("pause");
+        return 0;
+    }
     
-//     char *romPath = argv[1];
-    char *romPath = R"(C:\Users\Pablo\Desktop\rom\instr_timing.gb)";
+    char *romPath = argv[1];
+    //char *romPath = R"(C:\Users\Pablo\Desktop\rom\test.gb)";
 
     GameBoy gameboy;
     if (!gameboy.init(romPath)) {
